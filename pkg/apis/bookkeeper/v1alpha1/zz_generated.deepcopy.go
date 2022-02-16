928f80ea42234208558ac4d6a9e21c3ed373a98f
Alfred Landrum <alfred@leakybucket.org>
2022-02-15 18:40:32 -0800

    add toleration support for bookie pods
    Signed-off-by: Alfred Landrum <alfred@leakybucket.org>

--- pkg/apis/bookkeeper/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/bookkeeper/v1alpha1/zz_generated.deepcopy.go
@@ -146,6 +146,13 @@ func (in *BookkeeperClusterSpec) DeepCopyInto(out *BookkeeperClusterSpec) {
 		*out = new(bool)
 		**out = **in
 	}
+	if in.Tolerations != nil {
+		in, out := &in.Tolerations, &out.Tolerations
+		*out = make([]v1.Toleration, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
 	return
 }
 
